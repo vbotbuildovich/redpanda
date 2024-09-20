91a34bd70bf74ec3a86aa6146180a8d8f9c0ac2c
alexey bashtanov <alexey.bashtanov@redpanda.com>
2024-09-20 08:49:12 +0100

    tests/gtest_raft_rpunit: monitor_test_fixture honour leadership changes
    1) added a utility function to raft_fixture to execute a testing coro in
    retry_with_leader
    2) made both monitor_test_fixture tests use it
    
    (cherry picked from commit d1ecf0e4debae17fb500a8708f38ae5c0314baa5)

--- src/v/raft/tests/raft_fixture.h
+++ src/v/raft/tests/raft_fixture.h
@@ -19,6 +19,7 @@
 #include "model/timeout_clock.h"
 #include "raft/consensus_client_protocol.h"
 #include "raft/coordinated_recovery_throttle.h"
+#include "raft/errc.h"
 #include "raft/fwd.h"
 #include "raft/heartbeat_manager.h"
 #include "raft/recovery_memory_quota.h"
@@ -39,6 +40,7 @@
 #include <boost/range/irange.hpp>
 
 #include <ranges>
+#include <system_error>
 namespace raft {
 
 inline constexpr raft::group_id test_group(123);
@@ -526,6 +528,7 @@ public:
                 .then([&state] { return state.result; });
           });
     }
+
     template<typename Func>
     auto
     retry_with_leader(model::timeout_clock::time_point deadline, Func&& f) {
